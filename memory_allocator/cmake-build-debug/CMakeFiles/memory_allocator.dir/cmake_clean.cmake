5511fbc718dc6daa593dcc0bcfda760c14a924d7
Erdem <erdemsimsek.info@gmail.com>
2020-03-08 13:37:37 +0300

    proj created

---
+++ memory_allocator/cmake-build-debug/CMakeFiles/memory_allocator.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/memory_allocator.dir/main.c.o"
+  "libmemory_allocator.dll.a"
+  "memory_allocator.exe"
+  "memory_allocator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/memory_allocator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
