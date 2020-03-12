d04df965fb83016329d646635750d02272755887
Erdem <erdemsimsek.info@gmail.com>
2020-03-12 21:39:16 +0300

    PRoject Init

---
+++ file_searcher/cmake-build-debug/CMakeFiles/file_searcher.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/file_searcher.dir/main.c.o"
+  "file_searcher.exe"
+  "file_searcher.pdb"
+  "libfile_searcher.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/file_searcher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
