45fa1d26199e3ff51b2d9d202650b7eafd650066
Erdem <erdemsimsek.info@gmail.com>
2020-03-14 18:49:36 +0300

    Initilization of the Project

---
+++ thread_test/cmake-build-debug/CMakeFiles/thread_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/thread_test.dir/main.c.o"
+  "libthread_test.dll.a"
+  "thread_test.exe"
+  "thread_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/thread_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
