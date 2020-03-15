3d2fc64707a0c893e5fe1315d2178e68d2805dbc
Erdem <erdemsimsek.info@gmail.com>
2020-03-15 14:04:17 +0300

    project init

---
+++ posix_message/cmake-build-debug/CMakeFiles/posix_message.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/posix_message.dir/main.c.o"
+  "libposix_message.dll.a"
+  "posix_message.exe"
+  "posix_message.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/posix_message.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
